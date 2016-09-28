e3e8c710aa8a1d8a2da2626870fe516bf89793d3
BFKSKOLE\be09049802 <mbremyk@hotmail.no>
2016-09-28 13:47:13 +0200

    Added UselessInteractions - simple hei

---
+++ YahurrBot/UselessInteractions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+
+namespace YahurrBot
+{
+    class UselessInteractions
+    {
+        public void ParseCommands(string[] command, MessageEventArgs e)
+        {
+            switch(command[0])
+            {
+                case "hei":
+                    e.Channel.SendMessage("Hei, " + e.User.Mention + "!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
